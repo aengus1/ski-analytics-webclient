9d28453913bbe28b12cc52a7436383fc47669f38
Aengus McCullough <aengusmccullough@hotmail.com>
2017-12-11 21:18:11 -0800

    SKI-13 added filter and attribute sidebars, and controls to toggle them on off for both desktop and mobile layouts

--- src/app/activity-module/activity-module.component.ts
+++ src/app/activity-module/activity-module.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {SidebarComponent} from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-activity-module',
@@ -7,10 +8,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActivityModuleComponent implements OnInit {
 
+  @ViewChild(SidebarComponent) sidebar;
+
   summaryDisplay: Boolean = true;
+  sidebarOpen = false;
+  filterOpen = false;
+  attributeOpen = false;
+
   constructor() { }
+
 
   ngOnInit() {
   }
+
+  toggleSidebarFilter() {
+    if (this.filterOpen) {
+      this.filterOpen = false;
+      this.sidebarOpen = false;
+      this.sidebar.closeFilter();
+    } else {
+      this.attributeOpen = false;
+      this.filterOpen = true;
+      this.sidebarOpen = true;
+      this.sidebar.openFilter();
+    }
+  }
+
+  toggleMobileFilter() {
+    if (this.filterOpen) {
+      this.filterOpen = false;
+      this.attributeOpen = false;
+    } else {
+      this.attributeOpen = false;
+      this.filterOpen = true;
+    }
+  }
+
+  toggleMobileAttribute() {
+    if (this.attributeOpen) {
+      this.attributeOpen = false;
+      this.filterOpen = false;
+    } else {
+      this.filterOpen = false;
+      this.attributeOpen = true;
+    }
+  }
+
+
+   toggleSidebarAttribute() {
+     if (this.attributeOpen) {
+       this.attributeOpen = false;
+       this.sidebarOpen = false;
+       this.sidebar.closeAttribute();
+     } else {
+       this.filterOpen = false;
+       this.attributeOpen = true;
+       this.sidebarOpen = true;
+       this.sidebar.openAttribute();
+     }
+  }
+
+  receiveMessage($event) {
+    switch ($event) {
+      case 'closeSidebar':
+        this.sidebarOpen = false;
+        this.attributeOpen = false;
+        this.filterOpen = false;
+    }
+  }
+
+
+
+
 
 }
