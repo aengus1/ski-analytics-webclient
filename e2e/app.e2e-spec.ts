9df61ac4a13b167d800cd6a1c0c04c5d277e6284
Aengus McCullough <aengusmccullough@hotmail.com>
2017-12-19 12:51:05 -0800

    SKI-20 and SKI-15.  Regenerated project to get testing working.  was using an old version of angular/cli

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { SkiAnalyticsWebclientPage } from './app.po';
+import { AppPage } from './app.po';
 
-describe('ski-analytics-webclient App', function() {
-  let page: SkiAnalyticsWebclientPage;
+describe('ski-analytics-web App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new SkiAnalyticsWebclientPage();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
