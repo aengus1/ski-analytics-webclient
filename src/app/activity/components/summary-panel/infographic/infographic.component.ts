fb3ca3f544e402945e97f7f37fa4eea3acd1d089
Aengus McCullough <aengusmccullough@hotmail.com>
2018-06-10 21:54:02 -0700

    SKI-97 - fix responsive card layout. minor layout and UI improvements

--- src/app/activity/components/summary-panel/infographic/infographic.component.ts
+++ src/app/activity/components/summary-panel/infographic/infographic.component.ts
@@ -5,11 +5,13 @@ import {Component, Input} from '@angular/core';
   template:
     `<div  class="card" style="background-color: lightskyblue; border-color: #333;">
     <div class="card-title text-center">
-      <i class="fa" [ngClass]="icon" aria-hidden="true" ngbPopover="{{title}} {{uom}}" triggers="mouseenter:mouseleave"></i>
+      <i class="fa" [ngClass]="icon" aria-hidden="true" ngbPopover="{{description}}" popoverTitle="{{title}} ({{uom}})"
+         placement="right" container="body" triggers="mouseenter:mouseleave"></i>
+      <span class="text-body">&nbsp;{{title}}</span>
     </div>
     <div class="card-body">
     <ng-content></ng-content>
-      <small class="card-subtitle mb-2 text-muted pull-right">{{uom}}</small>
+      <!--<small class="card-subtitle mb-2 text-muted pull-right">{{uom}}</small>-->
     </div>
     </div>
   `,
@@ -19,6 +21,9 @@ export class InfographicComponent {
 
   @Input()
   private title: string;
+
+  @Input()
+  private description = '';
 
   @Input()
   private uom: string;
